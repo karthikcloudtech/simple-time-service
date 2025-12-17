8d51cdde3869b444a374f378d855760666fca201
karthik <karthik@local>
2025-12-17 23:52:36 +0530

    instance type

--- infra/terraform/modules/eks/main.tf
+++ infra/terraform/modules/eks/main.tf
@@ -111,12 +111,11 @@ resource "aws_eks_node_group" "main" {
   node_group_name = "${var.cluster_name}-node-group"
   node_role_arn = aws_iam_role.eks_node_role.arn
   subnet_ids     = var.subnet_ids
-  capacity_type  = "SPOT"
-  instance_types = ["t3.micro"]
+  instance_types = ["m7i-flex.large"]
   scaling_config {
     desired_size = 2
-    max_size     = 2
-    min_size     = 0
+    max_size     = 3
+    min_size     = 1
   }
   update_config {
     max_unavailable = 1
