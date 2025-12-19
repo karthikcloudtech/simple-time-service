b42a41ba7df6114e67e5ca8e48d9257b21db86d3
karthik <karthik@local>
2025-12-19 14:06:27 +0530

    adding EKS optimised AMI

--- infra/terraform/modules/eks/main.tf
+++ infra/terraform/modules/eks/main.tf
@@ -141,6 +141,7 @@ resource "aws_eks_node_group" "main" {
   node_role_arn = aws_iam_role.eks_node_role.arn
   subnet_ids     = var.subnet_ids
   instance_types = ["m7i-flex.large"]
+  ami_type       = "AL2023_x86_64_STANDARD"  # EKS-optimized Amazon Linux 2023 AMI for x86_64
   scaling_config {
     desired_size = 2
     max_size     = 3
