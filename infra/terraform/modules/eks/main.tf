791125f55d7c5c010aabdee8b688295629ef45f1
karthik <karthik@local>
2025-12-27 17:48:35 +0530

    removing dup im role ebs_csi_driver:

--- infra/terraform/modules/eks/main.tf
+++ infra/terraform/modules/eks/main.tf
@@ -252,40 +252,8 @@ resource "aws_iam_role_policy_attachment" "cluster_autoscaler" {
   }
 }
 
-# EBS CSI Driver IAM Role for PV support
-resource "aws_iam_role" "ebs_csi_driver" {
-  name = "${var.project_name}-ebs-csi-driver-role"
-  force_detach_policies = true
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Federated = aws_iam_openid_connect_provider.eks.arn
-      }
-      Action = "sts:AssumeRoleWithWebIdentity"
-      Condition = {
-        StringEquals = {
-          "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub" = "system:serviceaccount:kube-system:ebs-csi-controller-sa"
-          "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:aud" = "sts.amazonaws.com"
-        }
-      }
-    }]
-  })
-  tags = {
-    Name = "${var.project_name}-ebs-csi-driver-role"
-  }
-}
-
-resource "aws_iam_role_policy_attachment" "ebs_csi_driver" {
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-  role       = aws_iam_role.ebs_csi_driver.name
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
 # EBS CSI Driver Addon
+# Note: IAM role and policy are defined in iam_roles.tf
 resource "aws_eks_addon" "ebs_csi_driver" {
   cluster_name             = aws_eks_cluster.main.name
   addon_name               = "aws-ebs-csi-driver"
