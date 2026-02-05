c3bd1385336ae1c2e42b2ac4e781cea5fc609c9e
karthik <karthik@local>
2026-02-05 10:22:01 +0530

    added SPOT gitlab ec2 runner to tf

---
+++ infra/terraform/modules/ec2/gitlab_runner_ec2.tf
@@ -0,0 +1,60 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_security_group" "default" {
+  name   = "default"
+  vpc_id = data.aws_vpc.default.id
+}
+
+data "aws_subnets" "public" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+
+  filter {
+    name   = "map-public-ip-on-launch"
+    values = ["true"]
+  }
+    filter {
+    name   = "availability-zone"
+    values = ["us-east-1a"]
+  }
+}
+
+
+resource "aws_instance" "gitlab_runner_spot_instance" {
+  ami                         = "ami-00ac61dcca1e166d0"
+  instance_type               = "m7i-flex.large"
+  subnet_id                   = data.aws_subnets.public.ids[0]
+  vpc_security_group_ids      = [data.aws_security_group.default.id]
+  associate_public_ip_address = true
+
+  instance_market_options {
+    market_type = "spot"
+
+    spot_options {
+      spot_instance_type             = "one-time"
+      instance_interruption_behavior = "terminate"
+    }
+  }
+
+  tags = {
+    Name = "gitlab_runner_spot_instance"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket      = "simple-time-service-tf-state-prod"
+    key         = "ec2/terraform.tfstate"
+    region      = "us-east-1"
+    use_lockfile = true
+    encrypt     = true
+  }
+}
