6fd8aad39b6fe2c05639dc09a40f3aa26a7979c9
karthik <testmailkarthik@gmail.com>
2026-02-19 11:56:27 +0530

    fix sg of postgres

--- infra/terraform/modules/rds/main.tf
+++ infra/terraform/modules/rds/main.tf
@@ -10,7 +10,7 @@ resource "aws_security_group" "rds" {
     from_port       = 5432
     to_port         = 5432
     protocol        = "tcp"
-    security_groups = [var.eks_security_group_id]
+    security_groups = [var.node_security_group_id]
     description     = "Allow PostgreSQL from EKS cluster"
   }
 
